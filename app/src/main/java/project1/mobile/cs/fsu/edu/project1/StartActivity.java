205b96ebb857a259106c6a4f849447c06892a66e
abigailarreguin <32646740+abigailarreguin@users.noreply.github.com>
2019-04-03 21:58:43 -0400

    initial setup with StartActivity and HomeActivity

---
+++ app/src/main/java/project1/mobile/cs/fsu/edu/project1/StartActivity.java
@@ -0,0 +1,27 @@
+package project1.mobile.cs.fsu.edu.project1;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button loginButton = (Button)(findViewById(R.id.loginButton));
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+}
