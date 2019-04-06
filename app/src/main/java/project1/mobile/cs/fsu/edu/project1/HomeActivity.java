cc8d122ed104f827e0dda1faf068f1e9fb9b7c88
bennettbridges <bab14k@my.fsu.edu>
2019-04-06 18:50:24 -0400

    Added map

--- app/src/main/java/project1/mobile/cs/fsu/edu/project1/HomeActivity.java
+++ app/src/main/java/project1/mobile/cs/fsu/edu/project1/HomeActivity.java
@@ -1,7 +1,9 @@
 package project1.mobile.cs.fsu.edu.project1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void OnGoMap(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(intent);
     }
 }
