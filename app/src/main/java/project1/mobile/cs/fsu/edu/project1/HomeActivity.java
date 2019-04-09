e5d2f7b6186b78786fa436fee3b8f63fd9d44b6f
abigailarreguin <32646740+abigailarreguin@users.noreply.github.com>
2019-04-09 12:50:44 -0400

    adding menu to HomeActivity

--- app/src/main/java/project1/mobile/cs/fsu/edu/project1/HomeActivity.java
+++ app/src/main/java/project1/mobile/cs/fsu/edu/project1/HomeActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -27,6 +30,27 @@ private User mainUser;
     {
         Intent myintent = new Intent(this, MapsActivity.class);
         startActivity(myintent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuSettings:
+                // BRANDON - start settings fragment here
+                return true;
+            case R.id.menuLogout:
+                Intent myOtherIntent = new Intent(this, StartActivity.class);
+                startActivity(myOtherIntent);
+                return true;
+        }
+        return true;
     }
 
 }
